3df748730e62bea4c7777b0a94cddb190a322bc9
Jake <66699907+St8Frm-96@users.noreply.github.com>
2021-09-23 15:37:21 -0400

    updated

---
+++ moviedata.js
@@ -0,0 +1,120 @@
+const moviedata = [
+  {
+      id: 1,
+      text: "Iron Man"
+  },
+  {
+      id: 2,
+      text: "The Incredible Hulk"
+  },
+  {
+      id: 3,
+      text: "Iron Man 2"
+  },
+  {
+      id: 4,
+      text: "Thor"
+  },
+  {
+      id: 5,
+      text: "Captain America: The First Avenger"
+  },
+  {
+      id: 6,
+      text: "The Avengers"
+  },
+  {
+      id: 7,
+      text: "Iron Man 3"
+  },
+  {
+      id: 8,
+      text: "Thor: The Dark World"
+  },
+  {
+      id: 9,
+      text: "Captain America: The Winter Soldier"
+  },
+  {
+      id: 10,
+      text: "Guardians of the Galaxy"
+  },
+  {
+      id: 11,
+      text: "Avengers: Age of Ultron"
+  },
+  {
+      id: 12,
+      text: "Ant-Man"
+  },
+  {
+      id: 13,
+      text: "Captain America: Civil War"
+  },
+  {
+      id: 14,
+      text: "Doctor Strange"
+  },
+  {
+      id: 15,
+      text: "Guardians of the Galaxy Vol. 2"
+  },
+  {
+      id: 16,
+      text: "Spider-Man: Homecoming"
+  },
+  {
+      id: 17,
+      text: "Thor: Ragnarok"
+  },
+  {
+      id: 18,
+      text: "Black Panther"
+  },
+  {
+      id: 19,
+      text: "Avengers: Infinity War"
+  },
+  {
+      id: 20,
+      text: "Ant-Man and the Wasp"
+  },
+  {
+      id: 21,
+      text: "Captain Marvel"
+  },
+  {
+      id: 22,
+      text: "Avengers: Endgame"
+  },
+  {
+      id: 23,
+      text: "Spider-Man: Far From Home"
+  },
+  {
+      id: 24,
+      text: "Black Widow"
+  },
+  {
+      id: 25,
+      text: "Shang-Chi and the Legend of the Ten Rings"
+  },
+  {
+      id: 26,
+      text: "WandaVision"
+  },
+  {
+      id: 27,
+      text: "The Falcon and The Winter Soldier"
+  },
+  {
+      id: 28,
+      text: "Loki"
+  },
+  {
+      id: 29,
+      text: "What If...?"
+  }
+]
+
+export default moviedata;
